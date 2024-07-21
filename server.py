5d54927902469a85e9169e1f5ddd0d84e8c41f26
jefferson <jguamanbastidas@gmail.com>
2024-07-20 22:37:07 -0500

    Initial commit

--- server.py
+++ server.py
@@ -1,12 +1,23 @@
 from flask import Flask, request, jsonify
 import firebase_admin
 from firebase_admin import credentials, db
+from dotenv import load_dotenv
+import os
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener la ruta de las credenciales desde las variables de entorno
+cred_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
 # Configuración de Firebase
-cred = credentials.Certificate('proyectosensoresbd-firebase-adminsdk-c4fra-5da97dd022.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://proyectosensoresbd-default-rtdb.firebaseio.com/'
-})
+if cred_path:
+    cred = credentials.Certificate(cred_path)
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://proyectosensoresbd-default-rtdb.firebaseio.com/'
+    })
+else:
+    print("Error: No se encontró la ruta de las credenciales.")
 
 app = Flask(__name__)
 
@@ -30,4 +41,5 @@ def update_weight():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
 
